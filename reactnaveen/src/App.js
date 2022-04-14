72b3a608c03c414248d19f0654fa17d5fb4aec67
Naveen <veershetti.naveen@gmail.com>
2022-04-14 10:51:26 +0530

    myreact

---
+++ reactnaveen/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Resturant from './component/Basics/Resturant';
+
+const App = () => {
+  return (
+    <>
+    <Resturant/>
+    </>
+  )
+}
+
+
+export default App
